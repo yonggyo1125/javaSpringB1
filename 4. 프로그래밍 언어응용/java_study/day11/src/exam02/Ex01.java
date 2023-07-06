ccf893fb446b51722b3b0729deffd496561e6c9c
yonggyo1125 <yonggyo00@kakao.com>
2023-07-06 10:52:32 +0900

    강의소스

---
+++ 4. 프로그래밍 언어응용/java_study/day11/src/exam02/Ex01.java
@@ -0,0 +1,10 @@
+package exam02;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Box box = new Box();
+        box.put(new Apple()); // Object item = new Apple()
+
+
+    }
+}
