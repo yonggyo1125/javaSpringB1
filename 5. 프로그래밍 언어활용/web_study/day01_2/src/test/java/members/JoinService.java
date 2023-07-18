7dfa52a72ba0c6926d522cf8df6585107e7c7aaa
yonggyo1125 <yonggyo00@kakao.com>
2023-07-18 09:51:11 +0900

    강의 소스

--- 5. 프로그래밍 언어활용/web_study/day01_2/src/test/java/members/JoinService.java
+++ 5. 프로그래밍 언어활용/web_study/day01_2/src/test/java/members/JoinService.java
@@ -2,21 +2,17 @@ package members;
 
 public class JoinService {
 
-    private JoinValidator joinValidator = new JoinValidator();
+    private JoinValidator joinValidator;
+
+    public JoinService(JoinValidator joinValidator) {
+        this.joinValidator = joinValidator;
+    }
 
     public void join(Users user) {
 
         joinValidator.check(user);
 
-        String userId = user.getUserId();
-        if (userId == null || userId.isBlank()) {
-            throw new ValidationException("아이디를 입력하세요.");
-        }
-        
-        String userPw = user.getUserPw();
-        if (userPw == null || userPw.isBlank()) {
-            throw new ValidationException("비밀번호를 입력하세요.");
-        }
+
 
 
     }
