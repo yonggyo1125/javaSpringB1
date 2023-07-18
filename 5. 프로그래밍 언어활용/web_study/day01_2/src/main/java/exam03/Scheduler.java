8c94f9963eeee1f7ead7697df5a2f7102bf1920a
yonggyo1125 <yonggyo00@kakao.com>
2023-07-18 11:24:26 +0900

    강의 소스

---
+++ 5. 프로그래밍 언어활용/web_study/day01_2/src/main/java/exam03/Scheduler.java
@@ -0,0 +1,7 @@
+package exam03;
+
+public interface Scheduler {
+    String view(int year, int month, int day);
+
+    void print();
+}
--- 5. 프로그래밍 언어활용/web_study/day01_2/src/test/java/tests/MockTest1.java
+++ 5. 프로그래밍 언어활용/web_study/day01_2/src/test/java/tests/MockTest1.java
@@ -1,15 +1,55 @@
 package tests;
 
 import exam03.Calculator;
+import exam03.Scheduler;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class MockTest1 {
+
+    @Mock
+    Calculator cal;
+
+    @Mock
+    Scheduler scheduler;
 
     @Test
     void test1() {
-        Calculator cal = mock(Calculator.class);
+        //Calculator cal = mock(Calculator.class);
+        given(cal.add(10, 20)).willReturn(30);
+
+        int result = cal.add(10,20);
+        assertEquals(30, result);
+    }
+
+    @Test
+    void test2() {
+       // Scheduler scheduler = mock(Scheduler.class);
+        given(scheduler.view(2023, 2, 31))
+                .willThrow(IllegalArgumentException.class);
+
+        assertThrows(IllegalArgumentException.class,() -> {
+            scheduler.view(2023, 2, 31);
+        });
+    }
+
+    @Test
+    void test3() {
+        //Scheduler scheduler = mock(Scheduler.class);
+        willThrow(RuntimeException.class)
+                .given(scheduler)
+                .print();
+
+        assertThrows(RuntimeException.class, () -> {
+            scheduler.print();
+        });
     }
 
 }
