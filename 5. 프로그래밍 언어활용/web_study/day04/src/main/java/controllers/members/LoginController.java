6c17aba97c237385bdea152e8bce7fab0017f19e
yonggyo1125 <yonggyo00@kakao.com>
2023-07-21 10:15:29 +0900

    강의소스

---
+++ 5. 프로그래밍 언어활용/web_study/day04/src/main/java/controllers/members/LoginController.java
@@ -0,0 +1,22 @@
+package controllers.members;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/member/login")
+public class LoginController extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
---
+++ 5. 프로그래밍 언어활용/web_study/day04/src/main/webapp/member/login.jsp
@@ -0,0 +1,6 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<form method="post" action="login">
+    <input type="text" name="userId" placeholder="아이디">
+    <input type="password" name="userPw" placeholder="비밀번호">
+    <button type="submit">로그인</button>
+</form>
