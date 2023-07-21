b97b040fdbd272942427baa109fa379f6c071598
yonggyo1125 <yonggyo00@kakao.com>
2023-07-21 09:51:07 +0900

    강의소스

--- 5. 프로그래밍 언어활용/web_study/day04/src/main/java/controllers/members/JoinController.java
+++ 5. 프로그래밍 언어활용/web_study/day04/src/main/java/controllers/members/JoinController.java
@@ -8,13 +8,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/member/join")
 public class JoinController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+
+        out.write("<h1>상단</h1>");
 
         RequestDispatcher rd = req.getRequestDispatcher("/member/join.jsp");
+        //rd.include(req, resp);
         rd.forward(req, resp);
+
+        out.write("<h1>하단</h1>");
     }
 }
