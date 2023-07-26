58284a7df6565c71d59ccb4bf0ab4673c109711c
yonggyo1125 <yonggyo00@kakao.com>
2023-07-26 13:33:44 +0900

    수업 소스

--- 스프링보강/spring_study/exam03/src/main/java/exam03/config/ProxyCalculator.java
+++ 스프링보강/spring_study/exam03/src/main/java/exam03/config/ProxyCalculator.java
@@ -1,6 +1,24 @@
 package exam03.config;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
 @Aspect
 public class ProxyCalculator {
 
+    @Pointcut("execution(* exam03.aopex..*(..))")
+    public void publicTarget() {}
+
+    @Around("publicTarget()")
+    public Object process(ProceedingJoinPoint joinPoint) throws Throwable {
+        // proceed() // 핵심 기능 수행
+
+        Object result = joinPoint.proceed(); // factorial(..);
+
+        System.out.println("여기?");
+
+        return result;
+    }
 }
