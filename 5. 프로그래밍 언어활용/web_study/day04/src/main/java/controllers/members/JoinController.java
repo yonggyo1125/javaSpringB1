84535ba85965e398fca0d9f62bb61fd0ce8ebaa5
yonggyo1125 <yonggyo00@kakao.com>
2023-07-26 09:28:16 +0900

    강의소스

--- 5. 프로그래밍 언어활용/web_study/day04/src/main/java/controllers/members/JoinController.java
+++ 5. 프로그래밍 언어활용/web_study/day04/src/main/java/controllers/members/JoinController.java
@@ -9,20 +9,21 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 
 @WebServlet("/member/join")
 public class JoinController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html; charset=UTF-8");
-        PrintWriter out = resp.getWriter();
 
-        out.write("<h1>상단</h1>");
+        String[] addScript = { "member/form.js", "member/util.js" };
+        List<String> addCss = Arrays.asList("member/join.css", "member/util.css");
 
-        RequestDispatcher rd = req.getRequestDispatcher("/member/join.jsp");
-        //rd.include(req, resp);
+        req.setAttribute("addScript", addScript);
+        req.setAttribute("addCss", addCss);
+
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/member/join.jsp");
         rd.forward(req, resp);
-
-        out.write("<h1>하단</h1>");
     }
 }
--- 5. 프로그래밍 언어활용/web_study/day04/src/main/webapp/exam10/ex01.jsp
+++ 5. 프로그래밍 언어활용/web_study/day04/src/main/webapp/exam10/ex01.jsp
@@ -1,5 +1,5 @@
 <%@ page contentType="text/html; charset=UTF-8" %>
 <%@ taglib prefix="layout" tagdir="/WEB-INF/tags/layouts" %>
-<layout:common>
+<layout:main>
     <h1>본문!</h1>
-</layout:common>
+</layout:main>
