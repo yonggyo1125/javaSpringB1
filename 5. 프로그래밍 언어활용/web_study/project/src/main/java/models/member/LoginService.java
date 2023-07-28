2812d82d778ff945f8862857b0cc8f973e5eb78b
yonggyo1125 <yonggyo00@kakao.com>
2023-07-28 10:42:49 +0900

    수업 소스

--- 5. 프로그래밍 언어활용/web_study/project/src/main/java/models/member/LoginService.java
+++ 5. 프로그래밍 언어활용/web_study/project/src/main/java/models/member/LoginService.java
@@ -1,6 +1,8 @@
 package models.member;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import validators.Validator;
 
@@ -14,7 +16,7 @@ public class LoginService {
         this.memberDao = memberDao;
     }
 
-    public void login(HttpServletRequest request){
+    public void login(HttpServletRequest request, HttpServletResponse response){
 
         // 유효성 검사
         validator.check(request);
@@ -24,5 +26,22 @@ public class LoginService {
         Member member = memberDao.get(userId);
         HttpSession session = request.getSession();
         session.setAttribute("member", member);
+
+        // 아이디 저장 처리
+        if (response != null) {
+            String saveId = request.getParameter("saveId");
+            Cookie cookie = new Cookie("saveId", member.getUserId());
+            if (saveId == null) { // 아이디를 쿠키에서 삭제 
+                cookie.setMaxAge(0);
+            } else { // 아이디를 쿠키에 저장
+                cookie.setMaxAge(60 * 60 * 24 * 365); // 1년간 쿠키 저장
+            }
+
+            response.addCookie(cookie); // Set-Cookie: saveId=...
+        }
+    }
+
+    public void login(HttpServletRequest request) {
+        login(request, null);
     }
 }
