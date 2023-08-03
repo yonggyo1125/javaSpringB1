4d6dea4d3dc5c9a750be0ae345cd8269707f62bc
yonggyo1125 <yonggyo00@kakao.com>
2023-08-03 10:32:13 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam02/config/ProxyCache.java
+++ 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam02/config/ProxyCache.java
@@ -1,4 +1,34 @@
 package exam02.config;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Aspect
 public class ProxyCache {
+
+    private Map<Long, Object> cache = new HashMap<>();
+
+    @Pointcut("execution(* exam02.aopex..*(..))")
+    public void publicTarget() {}
+
+    @Around("publicTarget()")
+    public Object process(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        Object[] args = joinPoint.getArgs();
+        Long num = (Long)args[0];
+        if (cache.containsKey(num)) {
+            System.out.println("캐시 사용!!");
+            return cache.get(num); // 캐시에 저장된 결과 사용
+        }
+
+        Object result = joinPoint.proceed(); // 다음 프록시 호출 -> proceed() -> factorial(...)
+        cache.put(num, result); // 캐시에 저장
+        System.out.println("캐시 저장!!");
+        return result;
+    }
 }
