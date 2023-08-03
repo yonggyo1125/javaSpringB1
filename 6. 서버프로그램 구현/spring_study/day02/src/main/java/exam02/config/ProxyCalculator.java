b1d562a23dde91c31d0d94b736d013ad55f297a6
yonggyo1125 <yonggyo00@kakao.com>
2023-08-03 10:22:30 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam02/config/ProxyCalculator.java
+++ 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam02/config/ProxyCalculator.java
@@ -1,6 +1,7 @@
 package exam02.config;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
@@ -13,6 +14,17 @@ public class ProxyCalculator {
 
     @Around("publicTarget()")
     public Object process(ProceedingJoinPoint joinPoint) throws Throwable {
+        /** 메서드 정보 */
+        /*
+        Signature sig = joinPoint.getSignature();
+        System.out.println("호출한 메서드 정보 : " + sig.toLongString());
+        */
+        /** 매개변수로 사용된 값 */
+        /*
+        Object[] args = joinPoint.getArgs();
+        long num = (Long)args[0];
+        System.out.println("num=" + num);
+        */
         long stime = System.nanoTime(); // 공통 기능(추가)
         try {
             Object result = joinPoint.proceed(); // factorial 핵심 기능 대신 수행
