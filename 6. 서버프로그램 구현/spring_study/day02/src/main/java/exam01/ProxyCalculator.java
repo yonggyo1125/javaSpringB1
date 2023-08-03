89dc652b0a521135f78ef721eb7e9f2fba37d440
yonggyo1125 <yonggyo00@kakao.com>
2023-08-03 09:42:50 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam01/ProxyCalculator.java
+++ 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam01/ProxyCalculator.java
@@ -1,8 +1,24 @@
 package exam01;
 
 public class ProxyCalculator implements Calculator {
+
+    private Calculator cal;
+
+    public ProxyCalculator(Calculator cal) {
+        this.cal = cal;
+    }
+
     @Override
     public long factorial(long num) {
-        return 0;
+
+        long stime = System.nanoTime(); // 공통 추가 기능
+        try {
+            long result = cal.factorial(num); // 핵심 기능을 대신 수행
+
+            return result;
+        } finally {
+            long etime = System.nanoTime(); // 공통 추가 기능
+            System.out.println("걸린시간 : " + (etime - stime));
+        }
     }
 }
---
+++ 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam02/config/ProxyCalculator.java
@@ -0,0 +1,21 @@
+package exam02.config;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class ProxyCalculator {
+
+    @Pointcut("execution(* exam02.aopex..*(..))")
+    public void publicTarget() {}
+
+    @Around("publicTarget()")
+    public Object process(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        Object result = joinPoint.proceed(); // factorial 핵심 기능 대신 수행
+
+        return result;
+    }
+}
