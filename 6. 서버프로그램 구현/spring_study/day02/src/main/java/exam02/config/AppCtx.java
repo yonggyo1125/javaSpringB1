e7ec05f4aa0f2e05844f8b2c9c7e7cb99c42c25f
yonggyo1125 <yonggyo00@kakao.com>
2023-08-03 10:52:58 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam02/config/AppCtx.java
+++ 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam02/config/AppCtx.java
@@ -6,8 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@EnableAspectJAutoProxy
+//@EnableAspectJAutoProxy // 인터페이스 기반의 프록시(표준)
+@EnableAspectJAutoProxy(proxyTargetClass = true) // 하위 클래스 기반의 프록시 생성
 public class AppCtx {
+
+    @Bean
+    public ProxyCache proxyCache() {
+        return new ProxyCache();
+    }
 
     @Bean
     public ProxyCalculator proxyCalculator() {
---
+++ 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam02/main/Ex02.java
@@ -0,0 +1,32 @@
+package exam02.main;
+
+import exam02.aopex.*;
+import exam02.config.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+        Calculator cal = ctx.getBean(Calculator.class);
+
+        long result = cal.factorial(10);
+        System.out.println(result);
+
+        result = cal.factorial(10);
+        System.out.println(result);
+
+        result = cal.factorial(10);
+        System.out.println(result);
+
+        result = cal.factorial(10);
+        System.out.println(result);
+
+        result = cal.factorial(10);
+        System.out.println(result);
+
+        result = cal.factorial(10);
+        System.out.println(result);
+
+        ctx.close();
+    }
+}
