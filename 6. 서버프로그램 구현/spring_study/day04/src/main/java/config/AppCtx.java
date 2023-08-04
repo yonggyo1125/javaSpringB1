b9c2c45cacbf9acd03996226a98b540827a3c07d
yonggyo1125 <yonggyo00@kakao.com>
2023-08-04 09:47:13 +0900

    수업 소스

---
+++ 6. 서버프로그램 구현/spring_study/day04/src/main/java/config/AppCtx.java
@@ -0,0 +1,35 @@
+package config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+@ComponentScan("models")
+public class AppCtx {
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        DataSource ds = new DataSource();
+        ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        ds.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+        ds.setUsername("BOARD_B1");
+        ds.setPassword("_aA123456");
+
+        /** 커넥션 풀 설정 */
+        ds.setInitialSize(2);
+        ds.setMaxActive(10);
+        ds.setTestWhileIdle(true);
+        ds.setTimeBetweenEvictionRunsMillis(3000);
+        ds.setMinEvictableIdleTimeMillis(1000 * 30);
+
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
