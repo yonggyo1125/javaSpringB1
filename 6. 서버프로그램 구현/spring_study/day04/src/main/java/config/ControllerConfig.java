acf5840baf008ee758200ae6510ea074a9cef0c6
yonggyo1125 <yonggyo00@kakao.com>
2023-08-04 12:54:12 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/day04/src/main/java/config/ControllerConfig.java
+++ 6. 서버프로그램 구현/spring_study/day04/src/main/java/config/ControllerConfig.java
@@ -1,7 +1,17 @@
 package config;
 
+import controllers.HelloController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan({"controllers"})
 public class ControllerConfig {
+    /*
+    @Bean
+    public HelloController helloController() {
+        return new HelloController();
+    }
+     */
 }
