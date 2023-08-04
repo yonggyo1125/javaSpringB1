d91f815a975554c458fe50ac497f08803c5e137a
yonggyo1125 <yonggyo00@kakao.com>
2023-08-04 12:44:05 +0900

    수업 소스

---
+++ 6. 서버프로그램 구현/spring_study/day04/src/main/java/config/MvcConfig.java
@@ -0,0 +1,25 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@EnableWebMvc
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 정적 자원 경로
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/view/", ".jsp");
+    }
+}
