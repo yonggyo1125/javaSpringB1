7ee9be320e5b9eb3f92c072650cb9c7367fe5289
yonggyo1125 <yonggyo00@kakao.com>
2023-08-08 09:28:59 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/day04/src/main/java/config/MvcConfig.java
+++ 6. 서버프로그램 구현/spring_study/day04/src/main/java/config/MvcConfig.java
@@ -3,13 +3,17 @@ package config;
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.*;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+
+import java.util.ResourceBundle;
 
 @EnableWebMvc
 @Configuration
@@ -63,5 +67,14 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.viewResolver(thymeleafViewResolver());
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+        ms.setDefaultEncoding("UTF-8");
+        ms.setBasenames("messages.commons");
+
+        return ms;
     }
 }
