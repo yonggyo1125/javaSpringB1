97bfee8e0fb0d81f91623c054e8ef4ff724c236a
yonggyo1125 <yonggyo00@kakao.com>
2023-08-09 09:24:35 +0900

    작업 소스

--- 6. 서버프로그램 구현/spring_study/project/src/main/java/controllers/member/JoinController.java
+++ 6. 서버프로그램 구현/spring_study/project/src/main/java/controllers/member/JoinController.java
@@ -1,13 +1,19 @@
 package controllers.member;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/member/join")
+@RequiredArgsConstructor
 public class JoinController {
+
+    private final JoinValidator joinValidator;
+
     @GetMapping
     public String join() {
 
@@ -15,7 +21,9 @@ public class JoinController {
     }
 
     @PostMapping
-    public String joinPs() {
+    public String joinPs(JoinForm joinForm, Errors errors) {
+
+        joinValidator.validate(joinForm, errors);
 
         return "redirect:/member/login";
     }
---
+++ 6. 서버프로그램 구현/spring_study/project/src/main/java/controllers/member/JoinValidator.java
@@ -0,0 +1,37 @@
+package controllers.member;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class JoinValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) { // 검증하고자 하는 커맨드 객체 제한
+        return clazz.isAssignableFrom(JoinForm.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) { // 유효성 검사 진행
+        JoinForm joinForm = (JoinForm)target;
+
+        /** 필수 항목 검증 */
+        String userId = joinForm.getUserId();
+        String userPw = joinForm.getUserPw();
+        String userPwRe = joinForm.getUserPw();
+        String userNm = joinForm.getUserNm();
+        String email = joinForm.getEmail();
+
+        if (userId == null || userId.isBlank()) {
+            errors.rejectValue("userId", "Required", "아이디를 입력하세요.");
+        }
+
+        if (userPw == null || userPw.isBlank()) {
+            errors.rejectValue("userPw", "Required", "비밀번호를 입력하세요.");
+        }
+
+        if (userPwRe == null || userPwRe.isBlank()) {
+            errors.rejectValue("userPwRe", "Required", "비밀번호를 확인하세요.");
+        }
+    }
+}
