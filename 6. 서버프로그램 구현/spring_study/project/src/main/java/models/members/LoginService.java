187a7a42526dff04aba33d06b5d14a62ec4f0bf3
yonggyo1125 <yonggyo00@kakao.com>
2023-08-10 09:23:51 +0900

    작업 소스

--- 6. 서버프로그램 구현/spring_study/project/src/main/java/models/members/LoginService.java
+++ 6. 서버프로그램 구현/spring_study/project/src/main/java/models/members/LoginService.java
@@ -1,12 +1,30 @@
 package models.members;
 
 import controllers.member.LoginForm;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
 
 @Service
+@RequiredArgsConstructor
 public class LoginService {
+
+    private final LoginValidator loginValidator;
+    private final HttpSession session;
+    private final MemberDao memberDao;
+
     public void login(LoginForm loginForm, Errors errors) {
 
+        loginValidator.validate(loginForm, errors);
+
+        if (errors.hasErrors()) { // 기본 검증 실패시
+            return;
+        }
+        
+        // 검증 성공시 로그인 처리
+        String userId = loginForm.getUserId();
+        Member member = memberDao.get(userId);
+        session.setAttribute("member", member);
     }
 }
