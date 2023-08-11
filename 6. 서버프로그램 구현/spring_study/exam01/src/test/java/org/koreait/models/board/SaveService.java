ecd09a2c0ca2146ec1e2666d47ddddfb9021c5b5
yonggyo1125 <yonggyo00@kakao.com>
2023-08-11 11:45:32 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/exam01/src/test/java/org/koreait/models/board/SaveService.java
+++ 6. 서버프로그램 구현/spring_study/exam01/src/test/java/org/koreait/models/board/SaveService.java
@@ -9,9 +9,11 @@ import org.springframework.stereotype.Service;
 public class SaveService {
 
     private final BoardSaveValidator validator;
+    private final BoardDataDao boardDataDao;
 
     public void save(BoardDataForm data) {
         validator.check(data);
 
+        boardDataDao.save(data);
     }
 }
