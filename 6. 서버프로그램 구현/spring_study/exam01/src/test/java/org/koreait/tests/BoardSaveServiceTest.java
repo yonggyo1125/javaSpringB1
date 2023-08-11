4be25f40e41b984302ec0bed07a26a8b6bfa4d87
yonggyo1125 <yonggyo00@kakao.com>
2023-08-11 10:51:13 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/exam01/src/test/java/org/koreait/tests/BoardSaveServiceTest.java
+++ 6. 서버프로그램 구현/spring_study/exam01/src/test/java/org/koreait/tests/BoardSaveServiceTest.java
@@ -1,5 +1,6 @@
 package org.koreait.tests;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.koreait.controllers.board.BoardDataForm;
@@ -16,12 +17,21 @@ public class BoardSaveServiceTest {
     @Autowired
     private SaveService saveService;
 
+    private BoardDataForm boardData;
+
+    @BeforeEach
+    void init() {
+        boardData = new BoardDataForm();
+        boardData.setPoster("작성자");
+        boardData.setSubject("제목");
+        boardData.setContent("내용");
+    }
+
     @Test
     @DisplayName("게시글 작성,수정 성공시 예외가 발생하지 않음")
     void saveSuccessTest() {
-        BoardDataForm data = new BoardDataForm();
         assertDoesNotThrow(() -> {
-            saveService.save(data);
+            saveService.save(boardData);
         });
     }
 
