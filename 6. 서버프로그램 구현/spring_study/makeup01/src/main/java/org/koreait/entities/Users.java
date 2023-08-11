05053f33a20698dbcfeb2af359e613d0ed4b626e
yonggyo1125 <yonggyo00@kakao.com>
2023-08-11 16:02:28 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/makeup01/src/main/java/org/koreait/entities/Users.java
+++ 6. 서버프로그램 구현/spring_study/makeup01/src/main/java/org/koreait/entities/Users.java
@@ -2,12 +2,19 @@ package org.koreait.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 @Entity
+@Table(name="ES_MEMBER",
+        indexes = {
+            @Index(name="idx_regdt_desc", columnList = "regDt DESC"),
+                @Index(name="idx_email_mobile", columnList = "email, mobile")
+        })
 public class Users {
     @Id
     private Long userNo;
--- 6. 서버프로그램 구현/spring_study/makeup01/src/test/java/org/koreait/exam01/JPAEx1.java
+++ 6. 서버프로그램 구현/spring_study/makeup01/src/test/java/org/koreait/exam01/JPAEx1.java
@@ -1,6 +1,7 @@
 package org.koreait.exam01;
 
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.koreait.entities.Users;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +17,10 @@ public class JPAEx1 {
     @Autowired
     private EntityManager em;
 
-    @Test
-    void test1() {
+    private Users users;
+
+    @BeforeEach
+    void init() {
         Users users = new Users();
         users.setUserNo(1L);
         users.setUserId("user01");
@@ -26,7 +29,51 @@ public class JPAEx1 {
         users.setEmail("user01@test.org");
         users.setMobile("0100000");
 
-        em.persist(users); // 영속성에 추가 
+        em.persist(users); // 영속성에 추가 - 변화 감지
         em.flush(); // 쿼리 수행
+        //em.detach(users);
+    }
+
+    @Test
+    void test1() {
+        em.detach(users); // 영속성 분리 - 변화 감지 X
+
+        users.setUserNm("(수정)사용자01");
+        em.flush();
+
+        em.merge(users); // 분리된 영속성 -> 영속 상태 - 변화 감지
+        em.flush();
+
+        //em.remove(users); // 영속 상태 : 제거 상태
+        //em.flush();
+    }
+
+    @Test
+    void test2() {
+        Users users2 = em.find(Users.class, 1L);
+        System.out.println(users2);
+
+        Users users3 = em.find(Users.class, 1L);
+        System.out.println(users3);
+    }
+
+    @Test
+    void test3() {
+        Users users = new Users();
+        users.setUserNo(2L);
+        users.setUserId("user01");
+        users.setUserPw("12345678");
+        users.setUserNm("사용자01");
+        users.setEmail("user01@test.org");
+        users.setMobile("0100000");
+
+        em.persist(users); // 영속성에 추가 - 변화 감지
+        em.flush();
+
+        users.setUserNm("(수정)사용자01");
+        em.remove(users);
+
+        Users users2 = em.find(Users.class, 2L); // flush() -> select
+        System.out.println(users2);
     }
 }
