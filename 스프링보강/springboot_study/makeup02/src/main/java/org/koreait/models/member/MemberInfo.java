37dbcae3f4cf3ef2b85149a70aea3286bc4ccf7d
yonggyo1125 <yonggyo00@kakao.com>
2023-08-15 11:20:56 +0900

    수업 소스

--- 스프링보강/springboot_study/makeup02/src/main/java/org/koreait/models/member/MemberInfo.java
+++ 스프링보강/springboot_study/makeup02/src/main/java/org/koreait/models/member/MemberInfo.java
@@ -1,24 +1,37 @@
 package org.koreait.models.member;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
+@Data @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class MemberInfo implements UserDetails {
+
+    private Long userNo;
+    private String userId;
+    private String userPw;
+    private String userNm;
+    private Collection<GrantedAuthority> authorities;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return userPw;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return userId;
     }
 
     @Override
