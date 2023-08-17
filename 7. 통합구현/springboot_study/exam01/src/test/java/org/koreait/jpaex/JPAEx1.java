6bd99fe4745705d14997444ca52c3b07abd999fb
yonggyo1125 <yonggyo00@kakao.com>
2023-08-17 10:16:17 +0900

    수업 소스

--- 7. 통합구현/springboot_study/exam01/src/test/java/org/koreait/jpaex/JPAEx1.java
+++ 7. 통합구현/springboot_study/exam01/src/test/java/org/koreait/jpaex/JPAEx1.java
@@ -3,9 +3,12 @@ package org.koreait.jpaex;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
+import org.koreait.entities.Users;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 @Transactional
@@ -16,6 +19,15 @@ public class JPAEx1 {
 
     @Test
     void test1() {
+        Users user = Users.builder()
+                .userNo(1L)
+                .userId("user01")
+                .userPw("123456")
+                .userNm("사용자01")
+                .regDt(LocalDateTime.now())
+                .build();
 
+        em.persist(user); // 영속 상태 : 변화감지
+        em.flush();
     }
 }
