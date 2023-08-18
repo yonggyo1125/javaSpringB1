1e268c09c988096c66b8b6f838e5a488213f0d95
yonggyo1125 <yonggyo00@kakao.com>
2023-08-18 12:12:33 +0900

    수업 소스

---
+++ 7. 통합구현/springboot_study/exam01/src/test/java/org/koreait/jpaex/JPAEx6.java
@@ -0,0 +1,56 @@
+package org.koreait.jpaex;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.koreait.entities.BoardData;
+import org.koreait.entities.HashTag;
+import org.koreait.repositories.BoardDataRepository;
+import org.koreait.repositories.HashTagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations="classpath:application-test.properties")
+public class JPAEx6 {
+    @Autowired
+    private BoardDataRepository boardDataRepository;
+
+    @Autowired
+    private HashTagRepository hashTagRepository;
+
+    @BeforeEach
+    void init() {
+        List<HashTag> tags = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            HashTag tag = new HashTag();
+            tag.setTag("태그" + i);
+            tags.add(tag);
+        }
+
+        hashTagRepository.saveAllAndFlush(tags);
+
+        List<BoardData> items = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            BoardData item = BoardData.builder()
+                    .poster("작성자" + i)
+                    .subject("제목" + i)
+                    .content("내용" + i)
+                    .tags(tags)
+                    .build();
+            items.add(item);
+        }
+
+        boardDataRepository.saveAllAndFlush(items);
+    }
+
+    @Test
+    void test1() {
+
+    }
+}
