d8cc0c434ece0ab38e5df3b09ef6c8005e2506d3
yonggyo1125 <yonggyo00@kakao.com>
2023-08-21 11:39:26 +0900

    수업 소스

--- 7. 통합구현/board/src/test/java/org/koreait/tests/BoardViewTest.java
+++ 7. 통합구현/board/src/test/java/org/koreait/tests/BoardViewTest.java
@@ -1,6 +1,8 @@
 package org.koreait.tests;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.koreait.controllers.BoardForm;
 import org.koreait.models.board.BoardInfoService;
 import org.koreait.models.board.BoardSaveService;
@@ -10,6 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 @SpringBootTest
 @Transactional
@@ -32,5 +37,11 @@ public class BoardViewTest {
                 .content("내용1")
                 .build();
         saveService.save(boardForm);
+    }
+
+    @Test
+    @DisplayName("게시글 조회 성공시 응답 코드 200, 저장전 데이터와 조회된 데이터의 동일 여부 체크")
+    void test1() {
+        mockMvc.perform(get("/api/board/view/" + boardForm.getId()))
     }
 }
