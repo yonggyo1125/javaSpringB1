5b061c7048751e688c98391ecacda39f5d3ee3ce
yonggyo1125 <yonggyo00@kakao.com>
2023-08-21 11:50:50 +0900

    수업 소스

--- 7. 통합구현/board/src/test/java/org/koreait/tests/BoardViewTest.java
+++ 7. 통합구현/board/src/test/java/org/koreait/tests/BoardViewTest.java
@@ -1,9 +1,14 @@
 package org.koreait.tests;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.koreait.commons.rests.JSONData;
 import org.koreait.controllers.BoardForm;
+import org.koreait.entities.BoardData;
 import org.koreait.models.board.BoardInfoService;
 import org.koreait.models.board.BoardSaveService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +17,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.charset.Charset;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -41,7 +50,24 @@ public class BoardViewTest {
 
     @Test
     @DisplayName("게시글 조회 성공시 응답 코드 200, 저장전 데이터와 조회된 데이터의 동일 여부 체크")
-    void test1() {
-        mockMvc.perform(get("/api/board/view/" + boardForm.getId()))
+    void test1() throws Exception {
+        String body = mockMvc.perform(get("/api/board/view/" + boardForm.getId()))
+                    .andExpect(status().isOk())
+                    .andReturn().getResponse()
+                    .getContentAsString(Charset.forName("UTF-8"));
+
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+        JSONData<BoardData> result = om.readValue(body, new TypeReference<JSONData<BoardData>>() {});
+
+        assertEquals(boardForm.getSubject(), result.getData().getSubject());
+        assertEquals(boardForm.getContent(), result.getData().getContent());
+    }
+
+    @Test
+    @DisplayName("없는 게시글 조회시 응답 코드 400")
+    void test2() throws Exception {
+        mockMvc.perform(get("/api/board/view/" + System.currentTimeMillis()))
+                .andExpect(status().isBadRequest());
     }
 }
