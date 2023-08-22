ad5c35704dd541450c70bc44390535051510a140
yonggyo1125 <yonggyo00@kakao.com>
2023-08-22 09:49:10 +0900

    수업 소스

--- 7. 통합구현/springboot_study/exam02/src/main/java/org/koreait/models/member/MemberSaveService.java
+++ 7. 통합구현/springboot_study/exam02/src/main/java/org/koreait/models/member/MemberSaveService.java
@@ -1,4 +1,26 @@
 package org.koreait.models.member;
 
+import lombok.RequiredArgsConstructor;
+import org.koreait.constants.UserType;
+import org.koreait.controllers.member.JoinForm;
+import org.koreait.entities.Users;
+import org.koreait.repositories.UsersRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberSaveService {
+    private final UsersRepository repository;
+    private final PasswordEncoder encoder;
+
+    public void save(JoinForm joinForm) {
+        Users users = new ModelMapper().map(joinForm, Users.class);
+        users.setType(UserType.USER);
+
+        users.setUserPw(encoder.encode(joinForm.getUserPw()));
+
+        repository.saveAndFlush(users);
+    }
 }
